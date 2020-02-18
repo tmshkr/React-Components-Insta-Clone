5510b7d860b42bd2ef2662cb8f98743acc9d434f
Tim Shaker <tim@tmshkr.com>
2020-02-17 20:53:45 -0800

    likes working

--- src/components/PostsContainer/LikeSection.js
+++ src/components/PostsContainer/LikeSection.js
@@ -1,27 +1,27 @@
 // look at the likes on this component. Right now it is hard coded on line 20.
 // console.log your props and see how to add likes to this component so that it will update when you click the icon.
 // HINT: You will need to add an onClick event pass through your props and update the hard coded Likes
-import React from "react";
+import React, { useState } from "react";
 
 const LikeSection = props => {
+  const [liked, setLiked] = useState(false);
   return (
     <div>
-    <div
-      className="like-section"
-      key="likes-icons-container"
-    >
-      <div className="like-section-wrapper">
-        <i className="far fa-heart" />
+      <div className="like-section" key="likes-icons-container">
+        <div className="like-section-wrapper">
+          <i
+            className={`${liked ? "fas" : "far"} fa-heart`}
+            style={liked ? { color: "#f44336" } : null}
+            onClick={() => setLiked(!liked)}
+          />
+        </div>
+        <div className="like-section-wrapper">
+          <i className="far fa-comment" />
+        </div>
       </div>
-      <div className="like-section-wrapper">
-        <i className="far fa-comment" />
-      </div>
+      <p className="like-number">{props.likes} likes</p>
     </div>
-    <p className="like-number">
-      
-      likes</p>
-</div>
-  )
+  );
 };
 
 export default LikeSection;
--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -1,18 +1,21 @@
 //Complete the necessary code in this file
 // import useState
-import React from "react";
+import React, { useState } from "react";
 import Post from "./Post";
 import "./Posts.css";
-// import data 
+// import data
+import data from "../../dummy-data";
 
 const PostsPage = () => {
   // set up state for your data
   return (
     <div className="posts-container-wrapper">
       {/* map through data here to return a Post and pass data as props to Post */}
+      {data.map((post, i) => (
+        <Post key={i} post={post} />
+      ))}
     </div>
   );
 };
 
 export default PostsPage;
-
